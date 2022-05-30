6b1c82c3d09d43fa42bc248cf375685334c0a5bd
Zuoyu111 <ultarffft@gamil.com>
2022-05-30 12:54:12 +0800

    test

--- admin/src/router/index.js
+++ admin/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import HomeView from "@/views/HomeView";
+import CategoryEdit from "@/views/CategoryEdit";
 
 Vue.use(VueRouter)
 
@@ -8,16 +9,15 @@ const routes = [
   {
     path: '/',
     name: 'home',
-    component: HomeView
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    component: HomeView,
+    children: [
+      {
+        path:'/categories/create',
+        component: CategoryEdit
+      }
+    ]
   }
+
 ]
 
 const router = new VueRouter({
